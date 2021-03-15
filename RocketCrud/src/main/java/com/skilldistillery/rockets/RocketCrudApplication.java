33f5a6e762113cdcdfe431e89252e28713feb62b
erikskell <erikskell@gmail.com>
2021-03-15 10:16:51 -0500

    updated readme

--- RocketCrud/src/main/java/com/skilldistillery/rockets/RocketCrudApplication.java
+++ RocketCrud/src/main/java/com/skilldistillery/rockets/RocketCrudApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.rockets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RocketCrudApplication {
+public class RocketCrudApplication extends SpringBootServletInitializer {
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(RocketCrudApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(RocketCrudApplication.class, args);
